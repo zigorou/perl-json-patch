bb39d80d1575bb5782c216403ccc4aab75e1f9c2
Toru Yamaguchi <zigorou@cpan.org>
2013-02-18 01:14:18 +0900

    initial import

---
+++ lib/JSON/Patch.pm
@@ -0,0 +1,41 @@
+package JSON::Patch;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+JSON::Patch - ...
+
+=head1 SYNOPSIS
+
+  use JSON::Patch;
+
+=head1 DESCRIPTION
+
+JSON::Patch is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Toru Yamaguchi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    JSON::Patch
+);
+
+done_testing;
